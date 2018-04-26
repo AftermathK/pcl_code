6bbbb6d27c10e0f7aaa9c233379719e7a621b00c
David Paz <dpazruiz@ucsd.edu>
2018-04-26 13:11:35 -0700

    Delete pcl2ply.sh

--- pcl2ply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Converts all the *.pcd files in the current directory into .ply files
-# and places them in a directory(ply)
-mkdir ./ply
-
-for currFile in ./*.pcd; do
-	pcl_pcd2ply 0 "$currFile" "./ply/$(basename "${currFile%.*}")PLY.ply"
-done
